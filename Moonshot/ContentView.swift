a412972dcfae6718aa011e3642396523e11081be
JoshuaRosado <joshua06rosado@gmail.com>
2024-11-28 22:26:48 -0600

    image resizable options

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+//        Image("example")
+        Image(.example)
+            .resizable() // resize the image
+            .scaledToFit() // scaled to FIT the parent
+            .scaledToFill() // scaled to FILL parent
+            
+        // make this image 80% of the available width
+            .containerRelativeFrame(.horizontal){
+                size, axis in size * 0.8
+            }
+        
+        // make this image 90% of the available height
+            .containerRelativeFrame(.vertical){
+                size, axis in size * 0.9
+            }
+
     }
 }
 
