b78f9128683389305ae3d1af5442f7db6c5b8d06
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-02 08:45:34 -0600

    images added

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,9 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+
+struct Missions: View {
+    var destinations : [String]
+    var spaceships: [String]
+    var body: some View{
+        ScrollView{
+
+                Text("Our next mission are to \(destinations[0]) in \(spaceships[0])")
+                Spacer()
+                Text("After that, our next mission are to \(destinations[1]) in \(spaceships[1])")
+            
+        }
         
+    }
+}
+struct ContentView: View {
+    let destinations = ["moon", "Mars"]
+    let spaceships = ["Apollo 11", "SpaceX"]
+    var body: some View {
+        NavigationStack{
+            VStack{
+                NavigationLink("Missions"){
+                    Missions(destinations: destinations, spaceships: spaceships)
+                    
+                    
+                }
+               
+            }
+           
+        }
+        .navigationTitle("MoonShot")
     }
 }
 
