d24e2b2a30412b38886f2e3e71ea781df76f50b5
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-01 21:34:19 -0600

    JSON astronauts and missions added

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,62 +7,11 @@
 
 import SwiftUI
 
-struct Horizontal: View {
-    
-    // GridItem adapts to the space, giving it a minimum and a maximum
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120))
-    ]
-    var body: some View {
-        ScrollView(.horizontal){ // specify .horizontal
-            LazyHGrid(rows: layout){ // Horizontal = Rows
-                ForEach(0..<1000){
-                    Text("Item \($0)")
-                }
-            }
-        }
-    }
-}
-
-struct Vertical: View {
-
-let layout = [
-    GridItem(.adaptive(minimum: 80, maximum: 120))
-]
-    var body: some View {
-        ScrollView{ // Vertical does not need to be specified
-            LazyVGrid(columns: layout){ // Vertical = Columns
-                ForEach(0..<1000){
-                    Text("Item \($0)")
-                }
-            }
-        }
-    }
-    
-}
-
 struct ContentView: View {
-    var body: some View{
-        NavigationStack{
-            HStack(spacing: 50){
-                NavigationLink("Horizontal"){
-                    Horizontal()
-                    
-                }
-                
-                
-                NavigationLink("Vertical"){
-                    Vertical()
-                    
-                }
-                .navigationTitle("Direction")
-            }
-            .font(.title)
-            
-        }
+    var body: some View {
+        
     }
 }
-
 
 #Preview {
     ContentView()
