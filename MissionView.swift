9fe0ea6ecebaedc1d45c6ef7d7f891e3e3067b3f
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-11 12:01:14 -0600

    CrewView added for horizontal ScrollView

--- MissionView.swift
+++ MissionView.swift
@@ -58,39 +58,39 @@ struct MissionView: View {
             }
             .padding(.bottom)
             
-            
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(crew, id: \.role) { crewMember in
-                        NavigationLink{
-                            AstronautView(astronaut: crewMember.astronaut)
-                            
-                        } label: {
-                            HStack {
-                                Image(crewMember.astronaut.id)
-                                    .resizable()
-                                    .frame(width: 104, height: 72)
-                                    .clipShape(.capsule)
-                                    .overlay(
-                                        Capsule()
-                                            .strokeBorder(.white, lineWidth: 1)
-                                    )
-                                
-                                VStack(alignment: .leading) {
-                                    Text(crewMember.astronaut.name)
-                                        .foregroundStyle(.white)
-                                        .font(.headline)
-                                    
-                                    Text(crewMember.role)
-                                        .foregroundStyle(.white.opacity(0.5))
-                                }
-                            }
-                            
-                            .padding(.horizontal)
-                        }
-                    }
-                }
-            }
+            CrewView(crew: crew)
+//            ScrollView(.horizontal, showsIndicators: false) {
+//                HStack {
+//                    ForEach(crew, id: \.role) { crewMember in
+//                        NavigationLink{
+//                            AstronautView(astronaut: crewMember.astronaut)
+//                            
+//                        } label: {
+//                            HStack {
+//                                Image(crewMember.astronaut.id)
+//                                    .resizable()
+//                                    .frame(width: 104, height: 72)
+//                                    .clipShape(.capsule)
+//                                    .overlay(
+//                                        Capsule()
+//                                            .strokeBorder(.white, lineWidth: 1)
+//                                    )
+//                                
+//                                VStack(alignment: .leading) {
+//                                    Text(crewMember.astronaut.name)
+//                                        .foregroundStyle(.white)
+//                                        .font(.headline)
+//                                    
+//                                    Text(crewMember.role)
+//                                        .foregroundStyle(.white.opacity(0.5))
+//                                }
+//                            }
+//                            
+//                            .padding(.horizontal)
+//                        }
+//                    }
+//                }
+//            }
         }
         .navigationTitle(mission.displayName)
         .navigationBarTitleDisplayMode(.inline)
