984c63ca336139615ae3c01ec89a5d83e6d6d071
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-12 13:00:38 -0600

    GridView created

---
+++ ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  Moonshot
+//
+//  Created by Joshua Rosado Olivencia on 12/12/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
--- MissionView.swift
+++ MissionView.swift
@@ -59,38 +59,7 @@ struct MissionView: View {
             .padding(.bottom)
             
             CrewView(crew: crew)
-//            ScrollView(.horizontal, showsIndicators: false) {
-//                HStack {
-//                    ForEach(crew, id: \.role) { crewMember in
-//                        NavigationLink{
-//                            AstronautView(astronaut: crewMember.astronaut)
-//                            
-//                        } label: {
-//                            HStack {
-//                                Image(crewMember.astronaut.id)
-//                                    .resizable()
-//                                    .frame(width: 104, height: 72)
-//                                    .clipShape(.capsule)
-//                                    .overlay(
-//                                        Capsule()
-//                                            .strokeBorder(.white, lineWidth: 1)
-//                                    )
-//                                
-//                                VStack(alignment: .leading) {
-//                                    Text(crewMember.astronaut.name)
-//                                        .foregroundStyle(.white)
-//                                        .font(.headline)
-//                                    
-//                                    Text(crewMember.role)
-//                                        .foregroundStyle(.white.opacity(0.5))
-//                                }
-//                            }
-//                            
-//                            .padding(.horizontal)
-//                        }
-//                    }
-//                }
-//            }
+
         }
         .navigationTitle(mission.displayName)
         .navigationBarTitleDisplayMode(.inline)
